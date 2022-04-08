2bf65bd3a0224a20c245ac2e131ea92564969f86
Riddhividhale <87593958+Riddhividhale@users.noreply.github.com>
2022-04-08 12:41:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+}
+
+function setup(){
+    canvas= createCanvas(300,300)
+    canvas.center();
+  
+}
+function modelLoaded(){
+
+}
+
+function draw(){
+
+   
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
